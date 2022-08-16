f7880488d6438006e13a5f9d363bc3d23161b4a5
Martin Schinz <me@thinktainer.com>
2022-08-16 12:09:19 +0200

    removes cmp-cmdline which is annoying

--- .config/nvim/lua/thinktainer/plugins/cmp.lua
+++ .config/nvim/lua/thinktainer/plugins/cmp.lua
@@ -82,22 +82,6 @@ cmp.setup {
   },
 }
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline', keyword_length = 5 }
-  })
-})
-
 P.updateCapabilities = function(capabilities)
   local caps = require('cmp_nvim_lsp').update_capabilities(capabilities)
   return caps
